788ffb3515eb933d5312cdc3ec025be3673607e2
Frances Webb <frances.webb@cornell.edu>
2019-10-11 15:55:32 -0400

    Flesh out the code to create a record modification job
    I still want to implement a request builder model like I did in the copy dataset tool

---
+++ src/main/java/edu/cornell/library/folioimpl/interfaces/MatchingLogic.java
@@ -0,0 +1,9 @@
+package edu.cornell.library.folioimpl.interfaces;
+
+import java.util.Map;
+
+public interface MatchingLogic {
+
+  public boolean matches( Map<String,Object> object );
+
+}
--- src/main/java/edu/cornell/library/folioimpl/tools/SelectAndModifyRecords.java
+++ src/main/java/edu/cornell/library/folioimpl/tools/SelectAndModifyRecords.java
@@ -1,17 +1,25 @@
 package edu.cornell.library.folioimpl.tools;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import edu.cornell.library.folioimpl.interfaces.MatchingLogic;
 import edu.cornell.library.folioimpl.interfaces.ModificationLogic;
 import edu.cornell.library.folioimpl.objects.OkapiClient;
 
 public class SelectAndModifyRecords {
+  
+  private OkapiClient okapi;
+  private String endPoint;
+  private String query;
+  private MatchingLogic match;
+  private ModificationLogic mod;
 
-  public static void run( OkapiClient okapi, String endPoint, String query , ModificationLogic mod )
+  public static void run(OkapiClient okapi,String endPoint,String query,MatchingLogic match,ModificationLogic mod)
       throws IOException {
 
     int maxDataSetSize = 15000;
@@ -22,16 +30,36 @@ public class SelectAndModifyRecords {
       throw new UnsupportedOperationException(
           "Method available for endpoints of no more than "+maxDataSetSize+" matching records.");
     List<Map<String,Object>> sourceData = OkapiClient.resultsToList(sourceJson);
+    System.out.printf("%d records matching Okapi query.\n", sourceData.size());
+    if ( sourceData.size() == 0 )
+    { System.out.println("No changes made"); return; }
 
-    for ( Map<String,Object> record : sourceData ) {
+    List<Map<String,Object>> filteredSourceData;
+    if ( match == null )
+      filteredSourceData = sourceData;
+    else {
+      filteredSourceData = new ArrayList<>();
+      for ( Map<String,Object> record : sourceData )
+        if ( match.matches(record) )
+          filteredSourceData.add(record);
+    }
+    System.out.printf("%d records matching record filter rules.\n", filteredSourceData.size());
+    if ( filteredSourceData.size() == 0 )
+    { System.out.println("No changes made"); return; }
+
+
+    int updated = 0;
+    for ( Map<String,Object> record : filteredSourceData ) {
 
       // If modification changes record, push modified record back.
       if ( mod.modify( record )) {
         okapi.put(endPoint,record);
         System.out.println("Updated: "+endPoint+' '+mapper.writeValueAsString(record));
-      }
+        updated++;
+      } else
+        System.out.println("Not updated: "+endPoint+' '+mapper.writeValueAsString(record));
     }
-
+    System.out.printf("%d records updated.", updated);
   }
 
   private static ObjectMapper mapper = new ObjectMapper();
