374be1d2cb63d17184c1f1c2b72e94bd35b0edd0
Frances Webb <frances.webb@cornell.edu>
2020-03-11 09:12:38 -0400

    Assume item not in Folio yet if holding wasn't
    Because Folio's foreign key confirmation would have prevented the item being loaded before its holding, this assumption is mostly safe, though not entirely thread safe, as an item could theoretically have been attached to the holding in Folio in the time this method spends retrieving the items from Voyager and converting them to Folio objects.

--- src/main/java/edu/cornell/library/folioimpl/scripts/GetHoldingsForInstances.java
+++ src/main/java/edu/cornell/library/folioimpl/scripts/GetHoldingsForInstances.java
@@ -32,8 +32,8 @@ public class GetHoldingsForInstances {
       prop.load(in);
     }
 
-//    OkapiClient okapi = new OkapiClient( prop.getProperty("url4dmg"), prop.getProperty("token4dmg"), prop.getProperty("tenant4dmg") );
-    OkapiClient okapi = new OkapiClient( prop.getProperty("url4sb"), prop.getProperty("token4sb"), prop.getProperty("tenant4sb") );
+    OkapiClient okapi = new OkapiClient( prop.getProperty("url4dmg"), prop.getProperty("token4dmg"), prop.getProperty("tenant4dmg") );
+//    OkapiClient okapi = new OkapiClient( prop.getProperty("url4sb"), prop.getProperty("token4sb"), prop.getProperty("tenant4sb") );
 
     ReferenceData identifierTypes = new ReferenceData(okapi, "/identifier-types", "name");
     String localId = identifierTypes.getUuid("Local Identifier");
@@ -68,102 +68,37 @@ public class GetHoldingsForInstances {
     String lastHrid = null;
     for (Object instanceObject : instances) {
       Map<String, Object> instance = (HashMap<String, Object>) instanceObject;
-//      System.out.println(i);
-//      Map<String, Object> metadata = (HashMap<String, Object>) i.get("metadata");
-//      lastModDate = (String) metadata.get("updatedDate");
       lastHrid = (String) instance.get("hrid");
-
-      List<Object> identifiers = (ArrayList<Object>) instance.get("identifiers");
-      String title = ((String) instance.get("title"));
       String bibId = ((String) instance.get("hrid"));
-
-      List<String> potentialBibIds = new ArrayList<>();
-/*
-      for (Object identifier : identifiers) {
-        Map<String, String> id = (HashMap<String, String>) identifier;
-
-        // bib id location in Joe's instance stubs
-        if (id.get("identifierTypeId").equals(localId))
-          bibId = id.get("value");
-
-        // bib id location in instance generated from SRS q3.1
-        if (id.get("identifierTypeId").equals(sysCtrlNum))
-          if ( integerP.matcher(id.get("value")).matches() )
-            if ( isActuallyTheBibId( voyager, title, id.get("value")))
-              bibId = id.get("value");
-            else
-              potentialBibIds.add(id.get("value"));
-      }
-*/
-
-      if (bibId == null) {
-        if ( potentialBibIds.size() == 1 )
-          bibId = potentialBibIds.get(0);
-        else {
-          System.out.printf("bib id not identified: id:%s; title:%s\n",instance.get("id"),instance.get("title"));
-          skippedInstances.add((String)instance.get("id"));
-          continue;
-        }
-      }
 
       List<Holding> holdings = Holdings.getHoldingsForBibRecord(
           voyager, okapi, Integer.valueOf(bibId), (String)instance.get("id"));
-//      System.out.println("id=" + i.get("id") + "; bibid=" + bibId + "; holdingCount=" + holdings.size());
+      boolean holdingAlreadyInFolio = false;
       for (Holding h : holdings) {
-//        System.out.println("\t" + h.toString());
-//        String response = okapi.delete(holdingsEndPoint, h.getId());
-//        System.out.println(response);
-        String response;
         try {
           okapi.getRecord(holdingEndPoint, h.getId());
-          System.out.println(h.toString());
-          response = okapi.put(holdingEndPoint, h.getId(), h.toString());
+          okapi.put(holdingEndPoint, h.getId(), h.toString());
+          holdingAlreadyInFolio = true;
         } catch ( NoSuchObjectException e ) {
-          response = okapi.post(holdingEndPoint, h.toString());
+          okapi.post(holdingEndPoint, h.toString());
         }
         List<Item> items = itemLoader.getItemsForMfhdId(
             Integer.valueOf((String)h.holding.get("hrid")),voyager);
         for ( Item item : items ) {
-          try {
-            okapi.getRecord(itemEndPoint, item.getId());
-            response = okapi.put(itemEndPoint, item.getId(), item.toString());
-          } catch ( NoSuchObjectException e ) {
-            response = okapi.post(itemEndPoint, item.toString());
+          if ( ! holdingAlreadyInFolio ) {
+            okapi.post(itemEndPoint, item.toString());
+          } else {
+            try {
+              okapi.getRecord(itemEndPoint, item.getId());
+              okapi.put(itemEndPoint, item.getId(), item.toString());
+            } catch ( NoSuchObjectException e ) {
+              okapi.post(itemEndPoint, item.toString());
+            }
           }
         }
-//        if ( ! response.isEmpty() )
- //       System.out.println(response);
       }
-//      System.exit(0);
     }
     return lastHrid;
   }
-/*
-  private static boolean isActuallyTheBibId(Connection voyager, String title, String candidateBibId)
-      throws SQLException {
-
-    if ( bibTextQuery == null )
-      bibTextQuery = voyager.prepareStatement("SELECT title FROM bib_text WHERE bib_id = ?");
-    bibTextQuery.setInt(1, Integer.valueOf(candidateBibId));
-    title = title.trim().replaceAll("\\\\", "").replaceAll("\\s+"," ");
-    if (title.length() > 255) title = title.substring(0, 255);
-    try ( ResultSet rs = bibTextQuery.executeQuery() ) {
-      while ( rs.next() ) {
-        String dbTitle = new String(rs.getBytes(1),StandardCharsets.UTF_8).trim().replaceAll("\\s+"," ");
-        if ( dbTitle.equals(title) || dbTitle.equals(title+"."))
-          return true;
-        if ( title.length() - dbTitle.length() < 10 
-            && ( title.contains(dbTitle.substring(1,dbTitle.length()-1))))
-//                || (title+".").endsWith(dbTitle.substring(1))))
-          return true;
-//        System.out.println("Title mismatch:"+title+" <> "+dbTitle+" ("+candidateBibId+")");
-      }
-    }
-    return false;
-  }
-  static PreparedStatement bibTextQuery = null;
-  static Pattern integerP = Pattern.compile("^\\d+$");
-*/
-
 
 }
