ec5204b7530311e6b8338aced99e5574b3902700
Frances Webb <frances.webb@cornell.edu>
2020-03-13 10:10:57 -0400

    Clean up some unused stuff

--- src/main/java/edu/cornell/library/folioimpl/scripts/GetHoldingsForInstances.java
+++ src/main/java/edu/cornell/library/folioimpl/scripts/GetHoldingsForInstances.java
@@ -6,16 +6,15 @@ import java.rmi.NoSuchObjectException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+
 import edu.cornell.library.folioimpl.objects.Holding;
 import edu.cornell.library.folioimpl.objects.Item2Json;
 import edu.cornell.library.folioimpl.objects.Item2Json.Item;
 import edu.cornell.library.folioimpl.objects.OkapiClient;
-import edu.cornell.library.folioimpl.objects.ReferenceData;
 import edu.cornell.library.folioimpl.tools.Holdings;
 
 public class GetHoldingsForInstances {
@@ -35,34 +34,27 @@ public class GetHoldingsForInstances {
     OkapiClient okapi = new OkapiClient( prop.getProperty("url4dmg"), prop.getProperty("token4dmg"), prop.getProperty("tenant4dmg") );
 //    OkapiClient okapi = new OkapiClient( prop.getProperty("url4sb"), prop.getProperty("token4sb"), prop.getProperty("tenant4sb") );
 
-    ReferenceData identifierTypes = new ReferenceData(okapi, "/identifier-types", "name");
-    String localId = identifierTypes.getUuid("Local Identifier");
-    String sysCtrlNum = identifierTypes.getUuid("System control number");
-
-    List<String> skippedInstances = new ArrayList<>();
     try ( Connection voyager = DriverManager.getConnection(
         prop.getProperty("voyagerDBUrl"), prop.getProperty("voyagerDBUser"), prop.getProperty("voyagerDBPass")) ) {
 
       itemLoader = new Item2Json(voyager, okapi);
 
       List<Map<String,Object>> instances = okapi.queryAsList(instanceEndPoint,"id==* sortBy hrid",null);
-      String hridCursor = processInstanceBatch(voyager, okapi, instances, localId, sysCtrlNum, skippedInstances);
+      String hridCursor = processInstanceBatch(voyager, okapi, instances);
 
 //      String modDateCursor = "2019-08-14T00:25:52.702+0000";
       while (hridCursor != null) {
 
         instances = okapi.queryAsList(instanceEndPoint,
             String.format("hrid > \"%s\" sortBy hrid",hridCursor),null);
-        hridCursor = processInstanceBatch(voyager, okapi, instances, localId, sysCtrlNum, skippedInstances);
+        hridCursor = processInstanceBatch(voyager, okapi, instances);
       }
     }
-    System.out.println(skippedInstances.size()+ " instances where no holdings were loaded.");
-    System.out.println(String.join(", ", skippedInstances));
   }
 
   @SuppressWarnings("unchecked")
   private static String processInstanceBatch( Connection voyager, OkapiClient okapi,
-      List<Map<String,Object>> instances, String localId, String sysCtrlNum, List<String> skippedInstances)
+      List<Map<String,Object>> instances)
       throws NumberFormatException, SQLException, IOException {
 
     String lastHrid = null;
